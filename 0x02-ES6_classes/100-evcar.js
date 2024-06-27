e6f41e287cc23dee61902b9666c527f80dcd31b6
RidaOuledhaddou <reda7ouledhaddou@gmail.com>
2024-06-27 10:28:41 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,15 +3,15 @@ import Car from './10-car.js';
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
   get range() {
-    return this.range;
+    return this._range;
   }
 
   set range(value) {
-    this.range = value;
+    this._range = value;
   }
 
   cloneCar() {
