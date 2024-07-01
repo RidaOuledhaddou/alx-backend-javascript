83f20564d57e67dc189b6e20d95739b587af60ba
RidaOuledhaddou <reda7ouledhaddou@gmail.com>
2024-07-01 17:45:44 +0000

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const R = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const RR = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const RRR = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [R, RR, RRR];
+}
